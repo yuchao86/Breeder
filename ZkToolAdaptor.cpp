a3cdc51a809d20f2dff2e03b71f6596fec56a6ef
chao.yu@gree.net <yuchao86@gmail.com>
2012-11-16 17:54:29 +0800

    yuchao add the zookeeper c++ access tool API to Github

---
+++ ZkToolAdaptor.cpp
@@ -0,0 +1,12 @@
+#include "ZkToolAdaptor.h"
+
+///构造函数
+ZkToolAdaptor::ZkToolAdaptor(string const& strHost, uint32_t uiRecvTimeout):ZkAdaptor(strHost, uiRecvTimeout)
+{
+
+}
+///析构函数
+ZkToolAdaptor::~ZkToolAdaptor()
+{
+
+}
